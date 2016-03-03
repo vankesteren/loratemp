7aeec749d68a74bf0a5367d1d5cf42bf28119181
etothej <erikjan.vankesteren@gmail.com>
2016-03-03 23:23:02 +0100

    Split graphs
    Split Graphs, UI changes. Should make data reactive so I don't call it
    twice!

--- UI.R
+++ UI.R
@@ -4,34 +4,41 @@ library(stringr)
 library(plotly)
 
 shinyUI(fluidPage(theme = "bootstrap.css",
-                  
+
 footer = "(c) Erik-Jan van Kesteren",
 
 # Title
 fluidRow(
   column(8, offset = 2,
-         h1("Data from LoRa Sensor", align = "center")
+         h1("Data from LoRa Sensor", align = "center"),
+         br(),
+         hr(),
+         br()
         )
 ),
 
 fluidRow(
-  
+
   # Checkbox
   column(2,
         sidebarPanel(
           checkboxInput("update", "Automatically Update Data", value = T),
-          conditionalPanel("input.update == 1", 
-                           h6("Data is updated every 10 seconds", 
+          conditionalPanel("input.update == 1",
+                           h6("Data is updated every 10 seconds",
                               align = "center")
                            ),
           width = 12
         )
   ),
-  
+
   # Plot
-  column(10,
+  column(5,
          plotlyOutput("temp")
+  ),
+  column(5,
+        plotlyOutput("humid")
   )
+
 )
 
 ))
--- server.R
+++ server.R
@@ -1,46 +1,78 @@
-shinyServer(function(input, output, session) { 
+shinyServer(function(input, output, session) {
 
   # Refresh the data every 5 seconds
   autoInvalidate <- reactiveTimer(5000, session)
-  
-  
-  # I use Plotly to draw the interactive table
-  output$temp <- renderPlotly({
-    
+
+
+  dataInput <- reactive({
     # Data refresh
     autoInvalidate()
-    
+
     # Get the data from the things network api
     temp <- fromJSON("http://thethingsnetwork.org/api/v0/nodes/02030512/")
-    
+
     # Parse dataset
     ar <- strsplit(temp$data_plain, split = ", ")
     th <- matrix(as.numeric(unlist(ar)), ncol = 2, byrow = T)
     tm <- strptime(temp$time, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC") + 3600
-    
+
+    })
+  # I use Plotly to draw the interactive table
+  output$temp <- renderPlotly({
+
+    autoInvalidate()
+
+    # Get the data from the things network api
+    temp <- fromJSON("http://thethingsnetwork.org/api/v0/nodes/02030512/")
+
+    # Parse dataset
+    ar <- strsplit(temp$data_plain, split = ", ")
+    th <- matrix(as.numeric(unlist(ar)), ncol = 2, byrow = T)
+    tm <- strptime(temp$time, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC") + 3600
+
+
+
     # Draw plot
-    p <- plot_ly(x = tm, y = th[,2], name = "Temperature") %>%
-    add_trace(x = tm, y = th[,1], name = "Humidity", yaxis = "y2")
+    p <- plot_ly(x = tm, y = th[,2], name = "Temperature", color = "orange")
     l <- layout(p,
                 yaxis = list(
                   range = c(min(th[,2])-3,max(th[,2])+3),
                   title = "Temperature (Celcius)"
                   ),
-                yaxis2 = list(
-                  overlaying = "y",
-                  side = "right",
-                  range = c(25,55),
-                  title = "Humidity (%)"
-                ),
                 xaxis = list(
                   type = "Date",
                   title = "Time"
                 )
                 )
-    
+
   })
-  
-  
+  # I use Plotly to draw the interactive table
+  output$humid <- renderPlotly({
+
+    autoInvalidate()
+
+    # Get the data from the things network api
+    temp <- fromJSON("http://thethingsnetwork.org/api/v0/nodes/02030512/")
+
+    # Parse dataset
+    ar <- strsplit(temp$data_plain, split = ", ")
+    th <- matrix(as.numeric(unlist(ar)), ncol = 2, byrow = T)
+    tm <- strptime(temp$time, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC") + 3600
+
+    # Draw plot
+    p <- plot_ly(x = tm, y = th[,1], name = "Temperature")
+    l <- layout(p,
+                yaxis = list(
+                  range = c(25,55),
+                  title = "Humidity (%)"
+                  ),
+                xaxis = list(
+                  type = "Date",
+                  title = "Time"
+                )
+                )
+
+  })
 
 
 })
